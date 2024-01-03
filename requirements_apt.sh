491b5e70cded73ded7c02402ed1deb24af106e0f
kali <kali@kali.org>
2024-01-03 04:24:39 -0600

    Add projectdiscovery chaos tool to serach for subdomains to the installation list, next step complete the snrublist3r then dev chaos module

--- requirements_apt.sh
+++ requirements_apt.sh
@@ -4,8 +4,13 @@
 apt install whois
 
 # External tool installation
+
+## Snrublist3r installation
 rm -rf ~/BBP_sus_tools_base/
 mkdir ~/BBP_sus_tools_base
 cd ~/BBP_sus_tools_base
 git clone https://github.com/b3n-j4m1n/snrublist3r.git
 pip install -r ./snrublist3r/requirements.txt
+## Chaos ProjectDiscovery
+go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+# Note: Add export CHAOS_KEY=CHAOS_API_KEY in bash profile script
